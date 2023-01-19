59b54880aed63f825c2850bee1f7155e075ab3dc
Abram <israelvictory87@gmail.com>
2023-01-19 12:48:40 +0100

    Update: removed basic authentication from swagger security definitions

--- Univast/settings.py
+++ Univast/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
 
 SWAGGER_SETTINGS = {
     "SECURITY_DEFINITIONS": {
-        "Basic": {"type": "basic"},
         "Bearer": {"type": "apiKey", "name": "Authorization", "in": "header"},
     }
 }
