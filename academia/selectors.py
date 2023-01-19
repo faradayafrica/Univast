81d5999b9eb1a6da804232f1b20201f611df6b5d
Abram <israelvictory87@gmail.com>
2023-01-19 13:32:47 +0100

    Feat: implemented get_faculty selector

--- academia/selectors.py
+++ academia/selectors.py
@@ -2,7 +2,7 @@
 from rest_framework import exceptions
 
 # Own Imports
-from academia.models import School, Country
+from academia.models import School, Country, Faculty
 
 
 def get_school(code: str) -> str:
@@ -10,7 +10,7 @@ def get_school(code: str) -> str:
     This function checks if a school exist with the provided code,
     raises an NotFound except if it does not.
 
-    :param code: str - This is the school code that we want
+    :param code: This is the school code that we want
         to get the school for
     :type code: str
 
@@ -22,17 +22,31 @@ def get_school(code: str) -> str:
     return code
 
 
-def get_country(country: str) -> str:
+def get_country(name: str) -> str:
     """
     This function returns the country name if it exists,
     otherwise it raises a NotFound exception.
 
-    :param country: str - This is the parameter that will be passed in the URL
-    :type country: str
+    :param name: This is the name of the country we want
+    :type name: str
 
     :return: The country name.
     """
 
-    if not Country.objects.filter(name=country).exists():
+    if not Country.objects.filter(name=name).exists():
         raise exceptions.NotFound({"message": "Country not found!"})
-    return country
+    return name
+
+
+def get_faculty(name: str) -> str:
+    """
+    This function returns the faculty name if it exists,
+    otherwise it raises a NotFound exception.
+    
+    :param name: The name of the faculty we want
+    :type name: str
+    """
+    
+    if not Faculty.objects.filter(name=name).exists():
+        raise exceptions.NotFound({"message": "Faculty not found!"})
+    return name
