a0d8b0f653aa2b6a78ef76300442c1cb7b9e9782
Devgenix <emmaueln644@gmail.com>
2023-01-21 20:11:27 +0100

    modified allowed hosts setting.

--- Univast/settings.py
+++ Univast/settings.py
@@ -11,9 +11,7 @@ SECRET_KEY = get_random_secret_key()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = config(
-    "DJANGO_ALLOWED_HOSTS", "univast.faraday.africa", cast=Csv()
-)
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(" ")])
 
 # Application definition
 
