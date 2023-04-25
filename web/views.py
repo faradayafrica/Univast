ccc9420483a8a3421c8d04991382222e010c2f4e
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-04-26 00:30:00 +0100

    update/added airtable data to context

--- web/views.py
+++ web/views.py
@@ -2,6 +2,7 @@
 
 # Django imports
 from django.shortcuts import render
+from django.conf import settings
 
 # Local imports
 from academia.models import School
@@ -11,6 +12,17 @@ from academia.models import School
 
 def get_helpus (request):
     
+    AIRTABLE_API_KEY = settings.AIRTABLE_API_KEY
+    AIRTABLE_BASEID = settings.AIRTABLE_BASEID
+    AIRTABLE_TABLEID = settings.AIRTABLE_TABLEID
+    
     schools = School.objects.all().filter(unlisted=True)
     
-    return render(request, "soe.html", context={"schools": schools})
+    context = {
+        'schools': schools,
+        'AIRTABLE_API_KEY': AIRTABLE_API_KEY,
+        'AIRTABLE_BASEID': AIRTABLE_BASEID,
+        'AIRTABLE_TABLEID': AIRTABLE_TABLEID,
+    }
+    
+    return render(request, "soe.html", context)
