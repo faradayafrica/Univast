f3c4f7d1ed9c6bf3969b0da430db28a2d8083c1e
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-05-16 11:07:57 +0100

    update/move webhook to celery

---
+++ academia/tasks.py
@@ -0,0 +1,24 @@
+# Standard imports
+import requests
+
+# Celery tasks
+from celery import shared_task
+
+# Django imports
+from django.conf import settings
+
+@shared_task
+def dispatch_webhook(type, fid):
+    
+    # Dispatch webhook
+    API_URLS = settings.RECIEVER_WEBHOOK_API_URLS
+    webhook_urls = API_URLS.split(' ')
+
+    for webhook_url in webhook_urls:
+        
+        # Include ID in the request body
+        payload = {
+                    'fid': str(fid),
+                    'type': type
+                  }
+        response_hook = requests.post(webhook_url, data=payload)
