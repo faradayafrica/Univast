126bc54d97e65030d776302892be50a6d37f036e
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-05-16 01:05:25 +0100

    update/add path to get specific object

--- academia/urls.py
+++ academia/urls.py
@@ -2,28 +2,22 @@
 from django.urls import path
 
 # Own Imports
-from . import views
 from academia.views import (
-    CountryListAPIView,
+    home,
     SchoolListAPIView,
-    SchoolFacultyListAPIView,
+    CountryListAPIView,
+    GetObjectListAPIView,
     DepartmentListAPIView,
+    SchoolFacultyListAPIView,
 )
 
 app_name = "academia"
 
 urlpatterns = [
-    path("", views.home, name="homepage"),
+    path("", home, name="homepage"),
+    path("fetch", GetObjectListAPIView.as_view(), name="get_object"),
     path("countries", CountryListAPIView.as_view(), name="get_countries"),
     path("schools/<str:country_code>", SchoolListAPIView.as_view(), name="get_schools"),
-    path(
-        "faculties/<str:school_code>",
-        SchoolFacultyListAPIView.as_view(),
-        name="get_faculties",
-    ),
-    path(
-        "departments/<str:school_code>/<str:faculty_name>",
-        DepartmentListAPIView.as_view(),
-        name="get_departments",
-    ),
+    path("faculties/<str:school_code>", SchoolFacultyListAPIView.as_view(), name="get_faculties",),
+    path("departments/<str:school_code>/<str:faculty_name>", DepartmentListAPIView.as_view(), name="get_departments",),
 ]
