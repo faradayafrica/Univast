eef0dcc0f4681ad71e816b092cf5bbd0216e1b6a
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-05-16 21:40:38 +0100

    Update/added mini serializer

--- academia/serializers.py
+++ academia/serializers.py
@@ -75,11 +75,58 @@ class SchoolSerializer(serializers.ModelSerializer):
 
     def get_logo(self, obj: School) -> str:
         """This method gets the logo of the school."""
-        
+
         return obj.logo.url
 
+class MiniSchoolSerializer(serializers.ModelSerializer):
+    """
+    This seriliazer serilizes data for the School database model
+
+    :param: Request and/or object
+    :return: Serilized data specified by provided fields
+    """
+    logo = serializers.SerializerMethodField(
+        read_only=True, help_text="The school official logo."
+    )
+
+    class Meta:
+        model = School
+        fields = [
+            "id",
+            "name",
+            "code",
+            "logo"
+        ]
+        
+    def get_logo(self, obj: School) -> str:
+        """This method gets the logo of the school."""
+
+        return obj.logo.url
 
 class FacultySerializer(serializers.ModelSerializer):
+    """
+    This seriliazer serilizes data for the Faculty database model
+
+    :param: Request and/or object
+    :return: Serilized data specified by provided fields
+    """
+    school = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Faculty
+        fields = [
+            "id",
+            "name",
+            "school",
+        ]
+
+    def get_school(self, obj):
+        """"This takes the object and returns a serialised data of its school"""
+
+        serializer = MiniSchoolSerializer(obj.school, many=False)
+        return serializer.data
+
+class MiniFacultySerializer(serializers.ModelSerializer):
     """
     This seriliazer serilizes data for the Faculty database model
 
@@ -104,7 +151,22 @@ class DepartmentSerializer(serializers.ModelSerializer):
     """
 
     degree = DegreeSerializer(many=False, read_only=True)
+    faculty = serializers.SerializerMethodField(read_only=True)
+    school = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Department
-        fields = ["id", "name", "degree", "duration"]
+        fields = ["id", "name", "code", "duration",
+                  "degree", "faculty", "school"]
+
+    def get_faculty(self, obj):
+        """"This takes the object and returns a serialised data of its faculty"""
+
+        serializer = MiniFacultySerializer(obj.faculty, many=False)
+        return serializer.data
+
+    def get_school(self, obj):
+        """"This takes the object and returns a serialised data of its school"""
+
+        serializer = MiniSchoolSerializer(obj.school, many=False)
+        return serializer.data
