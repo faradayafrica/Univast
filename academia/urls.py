a352993690a7b7f29482e3fa3693936dfe2247c0
Abram <israelvictory87@gmail.com>
2023-05-16 21:30:43 +0100

    Update: format urls

--- academia/urls.py
+++ academia/urls.py
@@ -17,7 +17,19 @@ urlpatterns = [
     path("", home, name="homepage"),
     path("fetch", GetObjectListAPIView.as_view(), name="get_object"),
     path("countries", CountryListAPIView.as_view(), name="get_countries"),
-    path("schools/<str:country_code>", SchoolListAPIView.as_view(), name="get_schools"),
-    path("faculties/<str:school_code>", SchoolFacultyListAPIView.as_view(), name="get_faculties",),
-    path("departments/<str:school_code>/<str:faculty_name>", DepartmentListAPIView.as_view(), name="get_departments",),
+    path(
+        "schools/<str:country_code>",
+        SchoolListAPIView.as_view(),
+        name="get_schools",
+    ),
+    path(
+        "faculties/<str:school_code>",
+        SchoolFacultyListAPIView.as_view(),
+        name="get_faculties",
+    ),
+    path(
+        "departments/<str:school_code>/<str:faculty_name>",
+        DepartmentListAPIView.as_view(),
+        name="get_departments",
+    ),
 ]
