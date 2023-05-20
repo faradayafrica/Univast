0a9b8837e32b64aae2f1a18c92cc2a6a937e45c6
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-05-20 18:09:07 +0100

    changed redis env var and turned on debug

--- Univast/settings/development.py
+++ Univast/settings/development.py
@@ -1,6 +1,6 @@
 from Univast.settings.settings import *  # noqa
 
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = config("SECRET_KEY")  # noqa
 
@@ -14,38 +14,27 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        "LOCATION": config("CELERY_BROKER"), # in the format of redis://:password@host:port/db_number # noqa
+        "LOCATION": config("REDIS_URL"),  # in the format of redis://:password@host:port/db_number # noqa
         "TIMEOUT": None
     }
 }
 
-<<<<<<< HEAD
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [config("CELERY_BROKER")], # redis host must be in the format of redis://:password@host:port/db_number
-#         },
-#     },
-# }
-=======
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [config("CELERY_BROKER")], # redis host must be in the format of redis://:password@host:port/db_number # noqa
+            "hosts": [config("REDIS_URL")],  # redis host must be in the format of redis://:password@host:port/db_number # noqa
         },
     },
 }
->>>>>>> 06100ac40d48b5e68b377c3bc376fa4e7eb180a9
 
 # SSL Definition
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = False
 
 # Celery settings
-CELERY_BROKER_URL = config("CELERY_BROKER_URL") # noqa
-CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND") # noqa
+CELERY_BROKER_URL = config("CELERY_BROKER_URL")  # noqa
+CELERY_RESULT_BACKEND = config("CELERY_RESULT_BACKEND")  # noqa
 
 # Cloudinary Configuration
 cloudinary.config(
