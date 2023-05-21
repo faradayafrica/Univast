0063e10f21df65ca0c311ce73980607376c1f298
Nehemiah Onyekachukwu Emmanuel <emmanueln644@gmail.com>
2023-05-21 15:55:41 +0100

    refactored get to use ids not codes and names

--- academia/selectors.py
+++ academia/selectors.py
@@ -5,32 +5,32 @@ from rest_framework import exceptions
 from academia.models import School, Country, Faculty
 
 
-def get_school(code: str) -> str:
+def get_school(school_id: str) -> str:
     """
-    This function checks if a school exist with the provided code,
+    This function checks if a school exist with the provided id,
     raises an NotFound except if it does not.
 
-    :param code: This is the school code that we want
+    :param id: This is the school id that we want
         to get the school for
-    :type code: str
+    :type id: str
 
-    :return: The school code.
+    :return: The school id.
     """
 
-    if not School.objects.filter(code=code).exists():
+    if not School.objects.filter(id=school_id).exists():
         raise exceptions.NotFound({"message": "School not found!"})
-    return code
+    return school_id
 
 
 def get_country(country_id: str) -> str:
     """
-    This function returns the country code if it exists,
+    This function returns the country id if it exists,
     otherwise it raises a NotFound exception.
 
-    :param code: This is the code associated with the name of the country we want
-    :type code: str
+    :param id: This is the id associated with the name of the country we want
+    :type id: str
 
-    :return: The country code.
+    :return: The country id.
     """
 
     if not Country.objects.filter(id=country_id).exists():
@@ -38,15 +38,15 @@ def get_country(country_id: str) -> str:
     return country_id
 
 
-def get_faculty(name: str) -> str:
+def get_faculty(faculty_id: str) -> str:
     """
-    This function returns the faculty name if it exists,
+    This function returns the faculty id if it exists,
     otherwise it raises a NotFound exception.
 
-    :param name: The name of the faculty we want
-    :type name: str
+    :param id: The id of the faculty we want
+    :type id: str
     """
 
-    if not Faculty.objects.filter(name=name).exists():
+    if not Faculty.objects.filter(id=faculty_id).exists():
         raise exceptions.NotFound({"message": "Faculty not found!"})
-    return name
+    return faculty_id
--- academia/views.py
+++ academia/views.py
@@ -161,9 +161,9 @@ class SchoolListAPIView(generics.ListAPIView):
         """
         This API view retrieves the list of schools.
 
-        :param country_code: the code of country you wish to
+        :param country_id: the code of country you wish to
             get the list of available schools in.
-        \n:type country_code: str
+        \n:type country_id: str
         """
         # Check if the data is already cached
         key = f"schools_{country_id}"
@@ -197,7 +197,7 @@ class SchoolListAPIView(generics.ListAPIView):
     def get_queryset(self, country_id: str) -> QuerySet[School]:
         country_code = get_country(country_id)
         return self.queryset.filter(
-            country__country_id=country_code
+            country_id=country_id
         ).order_by("name")
 
 
@@ -211,17 +211,17 @@ class SchoolFacultyListAPIView(generics.ListAPIView):
         self.throttle_classes[0].request = request
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request: Request, school_code: str) -> Response:
+    def get(self, request: Request, school_id: str) -> Response:
         """
         This API view retrieves the list of faculties in a school.
 
-        :param school_code: the name of school you wish to
+        :param school_id: the id of school you wish to
             get the list of available faculties in.\n
-        \n:type school_code: str\n
+        \n:type school_id: str\n
         """
 
         # Check if the data is already cached
-        key = f"faculties_{school_code}"
+        key = f"faculties_{school_id}"
         data = cache.get(key)
 
         if data is not None:
@@ -233,7 +233,7 @@ class SchoolFacultyListAPIView(generics.ListAPIView):
 
             return Response(data=response, status=status.HTTP_200_OK)
 
-        faculties = self.get_queryset(school_code)
+        faculties = self.get_queryset(school_id)
         serializer = self.serializer_class(
             faculties, many=True, context={"request": request}
         )
@@ -248,9 +248,9 @@ class SchoolFacultyListAPIView(generics.ListAPIView):
         )
         return Response(data=response, status=status.HTTP_200_OK)
 
-    def get_queryset(self, code: str) -> QuerySet[Faculty]:
-        school_code = get_school(code)
-        return self.queryset.filter(school__code=school_code).order_by("name")
+    def get_queryset(self, school_id: str) -> QuerySet[Faculty]:
+        school_code = get_school(school_id)
+        return self.queryset.filter(school_id=school_id).order_by("name")
 
 
 class DepartmentListAPIView(generics.ListAPIView):
@@ -266,21 +266,21 @@ class DepartmentListAPIView(generics.ListAPIView):
         return super().dispatch(request, *args, **kwargs)
 
     def get(
-        self, request: Request, school_code: str, faculty_name: str
+        self, request: Request, school_id: str, faculty_id: str
     ) -> Response:
         """
         This API view retrieves the list of departments in a school.
 
-        :param school_code: the school (code) you wish to
+        :param school_id: the school (id) you wish to
             get the list of available departments in.
-        \n:type school_code: str\n
-        \n:param faculty_name: the name of faculty you wish
+        \n:type school_id: str\n
+        \n:param faculty_id: the id of faculty you wish
             to get the list of available departments in.
-        \n:type faculty_name: str\n
+        \n:type faculty_id: str\n
         """
 
         # Check if the data is already cached
-        key = f"departments_{school_code}_{faculty_name}"
+        key = f"departments_{school_id}_{faculty_id}"
         data = cache.get(key)
 
         if data is not None:
@@ -292,7 +292,7 @@ class DepartmentListAPIView(generics.ListAPIView):
 
             return Response(data=response, status=status.HTTP_200_OK)
 
-        schools = self.get_queryset(school_code, faculty_name)
+        schools = self.get_queryset(school_id, faculty_id)
         serializer = self.serializer_class(
             schools, many=True, context={"request": request}
         )
@@ -307,9 +307,9 @@ class DepartmentListAPIView(generics.ListAPIView):
         )
         return Response(data=response, status=status.HTTP_200_OK)
 
-    def get_queryset(self, code: str, faculty: str) -> QuerySet[Department]:
-        school_code = get_school(code)
-        faculty_name = get_faculty(faculty)
+    def get_queryset(self, school_id: str, faculty_id: str) -> QuerySet[Department]:
+        school_code = get_school(school_id)
+        faculty_name = get_faculty(faculty_id)
         return self.queryset.filter(
-            school__code=school_code, faculty__name=faculty_name
+            school_id=school_id, faculty_id=faculty_id
         ).order_by("name")
